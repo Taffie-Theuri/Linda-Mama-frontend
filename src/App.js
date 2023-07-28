94231c08e4f384a3836cb2faa83aa89634b8a967
+ <+>
2023-07-28 14:02:43 +0300

    feature:app -delete home and chat forum routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/LabResults/Home';
+
 import LabResults from './components/LabResults/LabResults';
-import ChatForum from './components/LabResults/ChatForum/ChatForum';
+
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          
           
           <Route path="/Lab-results" element={<LabResults />} />
-          <Route path="/Chat-forum" element={<ChatForum />} />
+          
         </Routes>
       </div>
     </Router>
