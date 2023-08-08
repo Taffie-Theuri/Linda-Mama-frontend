483b3743953da26f3b690ed2de23c2168f8eadd0
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-08-08 04:26:30 +0300

    feature:blogposts cards-on the read more button write logic to handle blog content display

--- src/Components/blogpost4.js
+++ src/Components/blogpost4.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import CommentForm from './CommentForm';
 
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
+
 const Post4 = ({ post, comments, onCommentSubmit }) => {
+  const [showFullContent, setShowFullContent] = useState(false);
+
+  const toggleContent = () => {
+    setShowFullContent(!showFullContent);
+  };
+
   return (
     <Card>
       <Card.Img variant="top" src={post.image} width={20} height={350} />
       <Card.Body>
         <Card.Title>{post.title}</Card.Title>
-        <Card.Text>{post.content}</Card.Text>
-        <a href="#" className="btn btn-primary">
-          Read More
-        </a>
-        <div>
+        <Card.Text>
+          {showFullContent ? post.content : post.content.slice(0, 200) + '...'}
+          <br />
+          <a onClick={toggleContent} className="btn btn-primary">
+            {showFullContent ? 'Read Less' : 'Read More'}
+          </a>
+          <div>
           {comments.map((comment) => (
             <div key={comment.id}>{comment.content}</div>
           ))}
           <CommentForm postId={post.id} onSubmit={onCommentSubmit} />
         </div>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
