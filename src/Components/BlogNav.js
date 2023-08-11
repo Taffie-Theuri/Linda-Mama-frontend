4edfd10991947e8173dd8a1089c3689d7a76f171
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-08-11 09:12:45 +0300

    patch:style-add styling to Navbar component

--- src/Components/BlogNav.js
+++ src/Components/BlogNav.js
@@ -1,13 +1,18 @@
  
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Form, FormControl } from 'react-bootstrap';
+// import { FontStyle } from "@cloudinary/url-gen/qualifiers";
  
 const BlogNav = () => {
     return (
         <div>
             <Navbar style={{
-                backgroundColor:"#FFC0CB"
+            backgroundColor: "#FFC0CB",
+            height: "80px", // Increase the height as desired
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
             }}>
                 <img
               src='https://cdn.vectorstock.com/i/1000x1000/38/47/pregnant-woman-stylized-symbol-vector-1753847.webp'
@@ -15,10 +20,10 @@ const BlogNav = () => {
               alt='LINDA-MAMA'
               loading='lazy'
             />
-                <Navbar.Brand href="#home" style={{color:"black", marginLeft:"10px"}}>LINDA-MAMA</Navbar.Brand>
+                <Navbar.Brand href="#home" style={{color:"black", marginLeft:"10px", fontStyle:"initial" }}>LINDA MAMA</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-end">
-                    <Nav>
+                    {/* <Nav>
                         <Nav.Link href="#home" style={{color:"black"}}>
                             BREAST FEEDING
                         </Nav.Link>
@@ -31,7 +36,7 @@ const BlogNav = () => {
                         <Nav.Link href="#contact" style={{color:"black"}}>
                             COVID AND PREGNANCY
                         </Nav.Link>
-                    </Nav>
+                    </Nav> */}
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="ml-auto"  />
                     </Form>
